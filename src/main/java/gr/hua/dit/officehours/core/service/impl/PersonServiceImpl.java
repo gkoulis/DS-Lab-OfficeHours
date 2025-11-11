1a408df341ef36d69f605fb69716a15c5790395f
Dimitris Gkoulis <gkould@gmail.com>
2025-11-11 19:26:49 +0200

    Added PersonServiceImpl (default implementation of PersonService)

---
+++ src/main/java/gr/hua/dit/officehours/core/service/impl/PersonServiceImpl.java
@@ -0,0 +1,91 @@
+package gr.hua.dit.officehours.core.service.impl;
+
+import gr.hua.dit.officehours.core.model.Person;
+import gr.hua.dit.officehours.core.model.PersonType;
+import gr.hua.dit.officehours.core.repository.PersonRepository;
+import gr.hua.dit.officehours.core.service.PersonService;
+import gr.hua.dit.officehours.core.service.model.CreatePersonRequest;
+import gr.hua.dit.officehours.core.service.model.PersonView;
+
+/**
+ * Default implementation of {@link PersonService}.
+ */
+public class PersonServiceImpl implements PersonService {
+
+    private final PersonRepository personRepository;
+
+    public PersonServiceImpl(final PersonRepository personRepository) {
+        if (personRepository == null) throw new NullPointerException();
+
+        this.personRepository = personRepository;
+    }
+
+    @Override
+    public PersonView createPerson(final CreatePersonRequest createPersonRequest) {
+        if (createPersonRequest == null) throw new NullPointerException();
+
+        // Unpack (we assume valid `CreatePersonRequest` instance)
+        // --------------------------------------------------
+
+        final PersonType type = createPersonRequest.type();
+        final String huaId = createPersonRequest.huaId().strip(); // remove whitespaces
+        final String firstName = createPersonRequest.firstName().strip();
+        final String lastName = createPersonRequest.lastName().strip();
+        final String emailAddress = createPersonRequest.emailAddress().strip();
+        final String mobilePhoneNumber = createPersonRequest.mobilePhoneNumber().strip();
+        final String rawPassword = createPersonRequest.rawPassword();
+
+        // Basic email address validation.
+        // --------------------------------------------------
+
+        if (!emailAddress.endsWith("@hua.gr")) {
+            throw new IllegalArgumentException("Only academic email addresses (@hua.gr) are allowed");
+        }
+
+        // --------------------------------------------------
+
+        // TODO huaId must be unique
+        // TODO emailAddress must be unique
+        // TODO mobilePhoneNumber must be unique
+
+        // --------------------------------------------------
+
+        // TODO use external service to validate huaId
+
+        // --------------------------------------------------
+
+        final String hashedPassword = rawPassword; // TODO Implement: encode password.
+
+        // Instantiate person.
+        // --------------------------------------------------
+
+        Person person = new Person();
+        person.setId(null); // auto generated
+        person.setHuaId(huaId);
+        person.setType(type);
+        person.setFirstName(firstName);
+        person.setLastName(lastName);
+        person.setEmailAddress(emailAddress);
+        person.setMobilePhoneNumber(mobilePhoneNumber);
+        person.setPasswordHash(hashedPassword);
+        person.setCreatedAt(null); // auto generated.
+
+        // Persist person (save/insert to database)
+        // --------------------------------------------------
+
+        person = this.personRepository.save(person);
+
+        // --------------------------------------------------
+
+        // TODO user external service to notify person.
+
+        // Map `Person` to `PersonView`.
+        // --------------------------------------------------
+
+        final PersonView personView = null; // TODO Implement.
+
+        // --------------------------------------------------
+
+        return personView;
+    }
+}

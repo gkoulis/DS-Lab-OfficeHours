a7c9837452ce4f75439275fc5fab0028c18ca369
Dimitris Gkoulis <gkould@gmail.com>
2025-11-25 16:29:27 +0200

    Replaced whitelabel error page with real global error handler (controller advice).

--- src/main/java/gr/hua/dit/officehours/web/TestController.java
+++ src/main/java/gr/hua/dit/officehours/web/TestController.java
@@ -1,19 +1,37 @@
 package gr.hua.dit.officehours.web;
 
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
- * REST controller for testing.
+ * Controller for <strong>Testing</strong>.
  */
-@RestController
+@Controller
 public class TestController {
+
+    // ΔΙΕΥΚΡΙΝΗΣΗ: Είναι controller για δοκιμές!
+    // Δεν έχει καμία σχέση με το configuration/implementation του error handling.
 
     public TestController() {}
 
-    @GetMapping(value = "/test", produces = MediaType.TEXT_PLAIN_VALUE)
+    /*
+    @GetMapping(value = "/test/error/404")
     public String test() {
-        return "test";
+        return "error/404";
     }
+
+    @GetMapping(value = "/test/error/error")
+    public String testErrorError() {
+        return "error/error";
+    }
+    */
+
+    @GetMapping(value = "/test/error/NullPointerException")
+    public String testErrorNullPointerException() {
+        final Integer a = null;
+        final int b = 0;
+        final int c = a + b; // Throws NullPointerException.
+        return null; // Unreachable.
+    }
+
 }
